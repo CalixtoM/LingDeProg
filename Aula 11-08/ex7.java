266c62396d815298c2acbbe510244138774cf3c6
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2021-08-17 20:59:15 -0300

    exercicio 7

---
+++ Aula 11-08/ex7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {  
+  public static void main(String[] args) { 
+    Scanner in = new Scanner (System.in);
+        System.out.println("digite um número: ");
+        int = n1 = in.nextInt();
+        System.out.println("digite um número: ");
+        int = n2 = in.nextInt();
+        if(n1 == n2){
+            System.out.println("Iguais");
+        }
+        else if(n1 > n2){
+            System.out.println(n1+", "+n2);
+        }
+    } 
+}
